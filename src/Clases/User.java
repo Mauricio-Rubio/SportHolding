8663894e4f61a97ec61a99b9098de2e92f77f0b2
IsaacAE <alcantara_estrada_kevin@ciencias.unam.mx>
2022-06-10 00:18:01 -0500

    Posible error

--- src/Clases/User.java
+++ src/Clases/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable{
     }
     
     public void setMoney(double money){
-        this.money=money;
+        this.mount=money;
     }
     @Override 
     public String toString(){
